08da8eff27f847a32be56cad0812f2b295ff89ac
Alexandro Jorge <alexandro.jorge@proway.treina>
2023-12-01 17:07:20 -0300

    terminei o crud contato agora farei o crud compromisso

---
+++ AgendaMVC/Controllers/CompromissoController.cs
@@ -0,0 +1,56 @@
+﻿using AgendaMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AgendaMVC.Controllers
+{
+	public class CompromissoController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View(Dados.Db.compromissos);
+		}
+
+		[HttpGet]
+		public IActionResult Create()
+		{
+			List<SelectListItem> Contatos = new List<SelectListItem>();
+			Contatos = Dados.Db.contatos.Select(c => new SelectListItem()
+			{ Text = c.Email, Value = c.Id.ToString() }).ToList();
+			ViewBag.Contatos = Contatos;
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Create(Compromisso compromisso)
+		{
+			if (Dados.Db.compromissos.Count > 0 ){
+				compromisso.Id = Dados.Db.compromissos.Max(c => c.Id) + 1;
+			}
+			else
+			{
+				compromisso.Id = 1;
+			}
+			compromisso.Contato = Dados.Db.contatos.FirstOrDefault(c => c.Id == compromisso.Contato.Id);
+			Dados.Db.compromissos.Add(compromisso);
+
+            return RedirectToAction("Index");
+		}
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            Models.Contato contato = Dados.Db.contatos.FirstOrDefault(ct => ct.Id == id);
+            return View(contato);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Models.Contato contato)
+        {
+            Models.Contato cont = Dados.Db.contatos.FirstOrDefault(ct => ct.Id == contato.Id);
+            cont.Nome = contato.Nome;
+            cont.Email = contato.Email;
+            cont.Telefone = contato.Telefone;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ AgendaMVC/Controllers/ContatoController.cs
@@ -0,0 +1,74 @@
+﻿using AgendaMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaMVC.Controllers
+{
+	public class ContatoController : Controller
+	{
+		static List<Models.Contato> contatos = new() {new Contato()
+            {
+                Id = 1,
+                Nome = "Maria",
+                Email = "Maria@gmail.com",
+                Telefone = "(47) 99278-2893"
+            } };
+
+        public IActionResult Index()
+        {
+            return View(Dados.Db.contatos);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Models.Contato contato)
+        {
+            contato.Id = Dados.Db.contatos.Count + 1;
+            Dados.Db.contatos.Add(contato);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            Models.Contato contato = Dados.Db.contatos.FirstOrDefault(ct => ct.Id == id);
+            return View(contato);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Models.Contato contato)
+        {
+            Models.Contato cont = Dados.Db.contatos.FirstOrDefault(ct => ct.Id == contato.Id);
+            cont.Nome = contato.Nome;
+            cont.Email = contato.Email;
+            cont.Telefone = contato.Telefone;
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int? id)
+        {
+            Models.Contato contato = Dados.Db.contatos.FirstOrDefault(ct => ct.Id == id);
+            return View(contato);
+        }
+        [HttpGet]
+        public IActionResult Deletar1(string email)
+        {
+            ViewData["Email"] = email;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult VoltarDeletar1(string email)
+        {
+            Dados.Db.contatos.RemoveAll(ct => ct.Email == email);
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
